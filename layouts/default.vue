34d5080a41b7211a27f856dcefe84d38f5e9e982
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-04-14 02:39:59 +0600

    updated year

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <main class="main-body">
     <div class="content-body">
       <div class="under-header-content bg-primary">
-        <h3>রমজানের সময়সূচী ২০২০(হিজরি ১৪৪১)</h3>
+        <h3>রমজানের সময়সূচী ২০২১(হিজরি ১৪৪২)</h3>
       </div>
       <nuxt class="content mb-4" />
       <footer class="footer text-center">
